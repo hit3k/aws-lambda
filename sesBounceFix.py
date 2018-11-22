546f3625fe034ae2a2a49775a80df52a3da1229a
hit3k <tanya.mikhno@gmail.com>
2018-11-22 14:54:39 +1100

    Update sesBounceFix.py

--- sesBounceFix.py
+++ sesBounceFix.py
@@ -1,4 +1,3 @@
-# Python 2.7
 from __future__ import print_function
 from base64 import b64decode
 
@@ -7,6 +6,7 @@ import os, json, boto3, urllib2, base64
 ## Environment variables
 # You can define Environment Variables as key-value pairs that are accessible from your function code.
 jenkins_url = os.environ['jenkins_url'] # plaintext
+token = os.environ['token'] # plaintext
 enc_username = os.environ['username'] # encrypted with KMS key
 enc_password = os.environ['password'] # encrypted with KMS key
 
@@ -15,8 +15,8 @@ enc_password = os.environ['password'] # encrypted with KMS key
 def callJenkins(email):
     print("Calling Jenkins _fix_email_bounce ( " + email + " ) ...")
     try:
-        # Connect to Jenkins and build a job
-        strUrl = jenkins_url + "/job/_fix_email_bounce/buildWithParameters?token=f90416498a64&Replace=true&email=" + email
+        # Connect to Jenkins and build a job 
+        strUrl = jenkins_url + "/job/_fix_email_bounce/buildWithParameters?token=" + token + "&Replace=true&email=" + email
         print(strUrl)
         request = urllib2.Request(strUrl)
 
@@ -25,34 +25,33 @@ def callJenkins(email):
         #base64string = base64.encodestring('%s:%s' % ('username', 'password')).replace('\n', '')
         base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
 
-        request.add_header("Authorization", "Basic %s" % base64string)
+        request.add_header("Authorization", "Basic %s" % base64string)   
         result = urllib2.urlopen(request)
     except Exception as e:
         print(e)
         print('Cannot connect to Jenkins server or run build job')
-        raise e
+        raise e  
 
 
 # Gets notifications from SNS topic ses_email_bounces and parses it for an email
 def lambda_handler(event, context):
     # print("Received event: " + json.dumps(event, indent=2))
     message = event['Records'][0]['Sns']['Message']
-    message = json.loads(message)
-
+    message = json.loads(message) 
+    
     # message is dict
-    if 'bounce' in message:
+    if 'bounce' in message: 
         bounce = message['bounce']
 
         # bounce is dict
         if 'bouncedRecipients' in bounce:
-            bouncedRecipients = bounce['bouncedRecipients']
-
+            bouncedRecipients = bounce['bouncedRecipients'] 
+            
             if len(bouncedRecipients) > 0:
                 for bouncedRecipient in bouncedRecipients:
                     email = bouncedRecipient['emailAddress']
                     print("SNS email: " , email)
                     callJenkins(email)
-        return "another job well done :-)"
+        return "another job well done :-)"            
     else:
         return "doesn't look like a geniune bounce AWS Notification to me :-/"
-
